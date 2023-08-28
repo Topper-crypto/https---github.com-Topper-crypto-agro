55e81e31911c77bc48cc0ad8b15432af4cedf0ad
Topper-crypto <tiadong@gmail.com>
2023-08-28 21:27:37 +0300

    fix-slider

---
+++ js/js-swipper.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+       
+  });
+
+  AOS.init();
