5c88dd06626d795d3a5ca7609e46d33c7dd0687a
Topper-crypto <tiadong@gmail.com>
2023-09-06 21:01:52 +0300

    fix

--- js/js-swipper.js
+++ js/js-swipper.js
@@ -1,34 +1,38 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  
+  grabCursor: true,
+ 
   
 
-    effect: "cube",
-    grabCursor: true,
-    cubeEffect: {
-      shadow: true,
-      slideShadows: true,
-      shadowOffset: 20,
-      shadowScale: 0.94,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
     },
+    
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-       
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+     
+});
 
-  AOS.init();
+AOS.init();
+/*   effect: "cube",
+grabCursor: true,
+cubeEffect: {
+  shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.94,
+}, */
