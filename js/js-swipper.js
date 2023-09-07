eab607acc2d809efa63ff4c95ec21ffc89c65099
Topper-crypto <tiadong@gmail.com>
2023-09-07 11:26:01 +0300

    button

--- js/js-swipper.js
+++ js/js-swipper.js
@@ -34,6 +34,26 @@ cubeEffect: {
 });
 
 AOS.init();
+
+/* var btn = $('#up'); //создаем переменную для кнопки
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 500) { //отслеживаем высоту от верха страницы в 100px
+        btn.addClass('show'); // присваиваем кнопке класс show для управления видимостью
+      } else {
+        btn.removeClass('show'); // если меньше 500px от верха страницы убираем класс show
+      }
+    });
+
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300'); //при клике на кнопку плавно прокручиваем до верха body
+    });
+ */
+
+
+
+
 /*   effect: "cube",
 grabCursor: true,
 cubeEffect: {
